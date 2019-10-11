dac03cf556a64cff4a5f0982097a272a2b026c56
kim <493319173@qq.com>
2019-10-11 19:18:08 +0800

    oijoit

--- stbk1/listtest.go
+++ stbk1/listtest.go
@@ -7,12 +7,12 @@ func main() {
 	var b [5]string
 	var c [5]int = [5]int{1, 2, 3}
 	var d = [...]int{1, 2, 3} // 长度根据chu
-	// hahahahahaha
+	// hahahahahahadfefasdf
 	fmt.Println(len(a))
 	fmt.Println(len(b))
 	fmt.Println(c[1])
 	fmt.Println(d)
-
+	fmt.Println("asdf")
 	for i, v := range a {
 		fmt.Println(i, v)
 	}
