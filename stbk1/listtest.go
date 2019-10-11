9e6268248a2c258ea2705f78205c54bf71af50f3
longhao54 <493319173@qq.com>
2019-10-12 01:25:26 +0800

    slice

--- stbk1/listtest.go
+++ stbk1/listtest.go
@@ -2,18 +2,29 @@ package main
 
 import "fmt"
 
+// 不同长度的数组 无法进行比较.  会报错
 func main() {
 	var a [5]int = [5]int{1, 2, 3, 4, 5}
 	var b [5]string
 	var c [5]int = [5]int{1, 2, 3}
-	var d = [...]int{1, 2, 3} // 长度根据chu
+	var d = [...]int{1, 2, 3} // 长度根据传入的数据决定
+	var e = [...]int{99: 2}   // 长度为100的数组. 前99项值为0 最后一个值为2
 	// hahahahahahadfefasdf
 	fmt.Println(len(a))
-	fmt.Println(len(b))
-	fmt.Println(c[1])
-	fmt.Println(d)
-	fmt.Println("asdf")
+	fmt.Println(len(b), e)
+	fmt.Println(len(c), len(d))
+
 	for i, v := range a {
 		fmt.Println(i, v)
 	}
+
+	type currency int
+	const (
+		USD currency = iota
+		EUR
+		RMB
+	)
+	symbol := [...]string{USD: "$", EUR: "aa", RMB: "#"} // 通过在上面用了iota 自增常量 . 相当于给数据每个下标都取了一个别名
+	fmt.Println(RMB, symbol[RMB])
+
 }
